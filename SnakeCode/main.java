2b633347e5a96c95443dd6de7941b14a8ed3c706
unknown <mgrombiz@gmail.com>
2023-11-18 19:01:04 +0100

    Frame | Panel | Snake

--- SnakeCode/main.java
+++ SnakeCode/main.java
@@ -1,7 +1,30 @@
 package SnakeCode;
+import javax.swing.*;
+import java.awt.*;
 
 public class main {
     public static void main(String[] args) {
-        
+
+        //Frame
+        JFrame frame = new JFrame();
+        frame.setSize(1000, 1000);
+        frame.setResizable(false);
+        frame.setTitle("Snake Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.black);
+        panel.setBounds(0, 0, 1000, 1000);
+        frame.add(panel);
+
+        //Snake
+        int x = 500; int y = 500;
+        JPanel snake = new JPanel();
+        snake.setBounds(x, y, 20, 20);
+        panel.add(snake);
+
+
+
+        frame.setVisible(true);
     }
 }
